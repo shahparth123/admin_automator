3f30e17e6f7e7bc0076d1d3f934a4615c75a76de
jay <LANET\jay@lcom41.lanet.com>
2015-03-31 14:04:45 +0530

    query maker half

--- application/views/service_generator/index.php
+++ application/views/service_generator/index.php
@@ -1,4 +1,5 @@
-
+<?php $attr=array('id'=>'query_maker'); 
+			echo form_open('api/index',$attr); ?>
 <div class="row">
     <div class="col-sm-8">
 
@@ -16,8 +17,7 @@
   </div>  
 
 </div>
-
-<div class="col-sm-4">
+    <div class="col-sm-4">
 
  <div class="panel panel-primary">
      <div class="panel-heading">
@@ -46,8 +46,82 @@
 
 </div>
 </div>
+    <div class="col-sm-10">
+
+	<div class="panel panel-primary" id="charts_env">
+
+	    <div class="panel-heading">
+		<div class="panel-title">Query Generator</div>
+	    </div>
+
+	    <div class="panel-body" id="tables">
+		<h4>Operation</h4>
+		<div class="radio-inline">
+		    <label><input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">SELECT</label>	
+		</div>
+		<div class="radio-inline">
+		    <label><input type="radio" name="optionsRadios" id="optionsRadios1" value="option2" checked="">INSERT</label>	
+		</div>
+		<div class="radio-inline">
+		    <label><input type="radio" name="optionsRadios" id="optionsRadios1" value="option3" checked="">UPDATE</label>	
+		</div>
+		<div class="radio-inline">
+		    <label><input type="radio" name="optionsRadios" id="optionsRadios1" value="option4" checked="">DELETE</label>	
+		</div>
+		<div class="col-sm-10">
+		    <h4>Fields</h4>
+		    <textarea class="form-control" id="field-ta" placeholder="Select Fields"></textarea>
+		</div>
+		<div class="col-sm-10">
+		    <h4>Conditions 			    <button id="add" type="button" class="btn btn-info">
+				<i class="entypo-plus"></i>
+			    </button>
+</h4>
+		    <div id="addconditions">
+			<div id="condition">
+			    <div class="col-sm-2">
+				<select class="form-control">
+				    <option>WHERE</option>
+				    <option>HAVING</option>
+				</select>
+			    </div>
+			    <div class="col-sm-3">
+				<input type="text" class="form-control" id="field-1" placeholder="field1">
+			    </div>
+			    <div class="col-sm-2">
+				<select class="form-control">
+				    <option value="=">=</option>
+				    <option value=">">&gt;</option>
+				    <option value=">=">&gt;=</option>
+				    <option value="<">&lt;</option>
+				    <option value="<=">&lt;=</option>
+				    <option value="!=">!=</option>
+				    <option value="LIKE">LIKE</option>
+				</select>
+			    </div>
+			    
+			    <div class="col-sm-3">
+				<input type="text" class="form-control" id="field-1" placeholder="field2">
+			    </div>
+			    <div class="col-sm-2">
+				<select class="form-control">
+				    <option>AND</option>
+				    <option>OR</option>
+				</select>
+			    </div>
+
+			</div>
+			
+		    </div>
+		</div>
+	    </div>
+
+	</div>  
+
+    </div>
 
 </div>
+<?php echo form_close(); ?>
 <script src="<?php echo base_url(); ?>/assets/js/switch/jquery.multi-select.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>/assets/js/switch/jquery.quicksearch.js" type="text/javascript"></script>
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/multi-select.css">
@@ -61,7 +135,7 @@
         url: url2,
         dataType: "json",
         success: function (data) {
-            var html1 = '<div class="col-md-4 tablename" id = "'+tablename+'"><div class="sorted"><div class="panel panel-info" data-collapsed="0"><!-- panel head --><div class="panel-heading"><div class="panel-title">Draggable Portlet 3</div></div><!-- panel body --><div class="panel-body">';
+            var html1 = '<div class="col-md-4 tablename" id = "'+tablename+'"><div class="sorted"><div class="panel panel-info" data-collapsed="0"><!-- panel head --><div class="panel-heading"><div class="panel-title">'+tablename+'</div></div><!-- panel body --><div class="panel-body">';
 
             jQuery.each(data, function (k,v) {
                 html1 += '<div class="form-group">';
@@ -83,6 +157,10 @@
         });                              
 }
 $(document).ready(function () {
+
+jQuery('#add').on('click', function(){
+   jQuery('#addconditions').append($('#condition').html());
+});
     var url1 = '<?php echo base_url(); ?>api/tables';
 
     $.ajax({
