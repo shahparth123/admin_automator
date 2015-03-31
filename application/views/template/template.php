2d55c186e927563edf88811097d15117ebad0baf
jay <LANET\jay@lcom41.lanet.com>
2015-03-31 10:32:38 +0530

    change in bootstrap js

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -11,19 +11,19 @@
 	<title><?php echo $title;?></title>
 	
 
-	<link rel="stylesheet" href="<?php echo base_url();?>/assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>/assets/css/font-icons/entypo/css/entypo.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/font-icons/entypo/css/entypo.css">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
-	<link rel="stylesheet" href="<?php echo base_url();?>/assets/css/bootstrap.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>/assets/css/neon-core.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>/assets/css/neon-theme.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>/assets/css/neon-forms.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>/assets/css/custom.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/neon-core.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/neon-theme.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/neon-forms.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/custom.css">
 
-	<script src="<?php echo base_url();?>/assets/js/jquery-1.11.0.min.js"></script>
+	<script src="<?php echo base_url();?>assets/js/jquery-1.11.0.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
-<script src="<?php echo base_url();?>/assets/js/ui-bootstrap-tpls-0.10.0.min.js"></script>
-<script src="<?php echo base_url();?>/assets/js/app.js"></script>
+<script src="<?php echo base_url();?>assets/js/ui-bootstra-tpls-0.10.0.min.js"></script>
+<script src="<?php echo base_url();?>assets/js/app.js"></script>
 
 	<!--[if lt IE 9]><script src="assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
 	
--- application/views/user/changepassword.php
+++ application/views/user/changepassword.php
@@ -8,7 +8,7 @@
 	<meta name="description" content="Neon Admin Panel" />
 	<meta name="author" content="" />
 	
-	<title>Neon | Forgot Password</title>
+	<title>Change Password</title>
 	
 
 	<link rel="stylesheet" href="<?php echo base_url();?>/assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
@@ -34,11 +34,10 @@
 </head>
 <body class="page-body login-page login-form-fall" data-url="http://neon.dev">
 
-
 <!-- This is needed when you send requests via Ajax --><script type="text/javascript">
 var baseurl = '';
 </script>
-	
+
 <div class="login-container">
 	
 	<div class="login-header login-caret">
@@ -49,7 +48,7 @@ var baseurl = '';
 				<img src="<?php echo base_url();?>/assets/images/logo@2x.png" width="120" alt="" />
 			</a>
 			
-			<p class="description">Enter your email, and we will send the reset link.</p>
+			<p class="description">Dear User Please Enter New Password</p>
 			
 			<!-- progress bar indicator -->
 			<div class="login-progressbar-indicator">
@@ -68,57 +67,48 @@ var baseurl = '';
 		
 		<div class="login-content">
 			
-			<form method="post" role="form" id="form_forgot_password">
+			<div class="form-login-error">
+				<h3>Invalid login</h3>
+				<p><strong>User Name</strong>/<strong>Password</strong> is invalid.</p>
+			</div>
+			
+			<?php $attr = array('id'=> "form_login");
+			     echo form_open('user/changepassword',$attr); ?>
 				
-				<div class="form-forgotpassword-success">
-					<i class="entypo-check"></i>
-					<h3>Reset email has been sent.</h3>
-					<p>Please check your email, reset password link will expire in 24 hours.</p>
-				</div>
-				
-				<div class="form-steps">
+				<div class="form-group">
 					
-					<div class="step current" id="step-1">
-					    <?php $attr = array('id'=> "form_forgotpassword");
-					 echo form_open('user/fogotpassword',$attr); ?>
-						<div class="form-group">
-							<div class="input-group">
-								<div class="input-group-addon">
-									<i class="entypo-mail"></i>
-								</div>
-								<?php 
-								$info = array('name'=> 'email','id'=> 'email','placeholder'=>'Email','class'=>'form-control');
-								echo form_input($info); ?>
-								<!--<input type="text" class="form-control" name="email" id="email" placeholder="Email" data-mask="email" autocomplete="off" />-->
-							</div>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="entypo-key"></i>
 						</div>
 						
-						<div class="form-group">
-							<button type="submit" class="btn btn-info btn-block btn-login">
-								Complete Registration
-								<i class="entypo-right-open-mini"></i>
-							</button>
-						</div>
-					<?php echo form_close(); ?>
+						<input type="password" class="form-control" name="password" id="password" placeholder="Password" autocomplete="off" />
 					</div>
-					
+				
 				</div>
 				
-			</form>
+				<div class="form-group">
+					
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="entypo-key"></i>
+						</div>
+						
+						<input type="password" class="form-control" name="password" id="password" placeholder="Confirm Password" autocomplete="off" />
+					</div>
+				
+				</div>
+				
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary btn-block btn-login">
+						<i class="entypo-login"></i>
+						Submit
+					</button>
+				
+						
+			<?php echo form_close(); ?>
 			
 			
-			<div class="login-bottom-links">
-				
-				<a href="<?php echo base_url();?>user/login" class="link">
-					<i class="entypo-lock"></i>
-					Return to Login Page
-				</a>
-				
-				<br />
-				
-				<a href="#">ToS</a>  - <a href="#">Privacy Policy</a>
-				
-			</div>
 			
 		</div>
 		
@@ -129,14 +119,13 @@ var baseurl = '';
 
 	<!-- Bottom Scripts -->
 	<script src="<?php echo base_url();?>/assets/js/gsap/main-gsap.js"></script>
-	<script src="assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
+	<script src="<?php echo base_url();?>/assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
 	<script src="<?php echo base_url();?>/assets/js/bootstrap.js"></script>
 	<script src="<?php echo base_url();?>/assets/js/joinable.js"></script>
 	<script src="<?php echo base_url();?>/assets/js/resizeable.js"></script>
 	<script src="<?php echo base_url();?>/assets/js/neon-api.js"></script>
 	<script src="<?php echo base_url();?>/assets/js/jquery.validate.min.js"></script>
-	<script src="<?php echo base_url();?>/assets/js/neon-forgotpassword.js"></script>
-	<script src="<?php echo base_url();?>/assets/js/jquery.inputmask.bundle.min.js"></script>
+	<script src="<?php echo base_url();?>/assets/js/neon-login.js"></script>
 	<script src="<?php echo base_url();?>/assets/js/neon-custom.js"></script>
 	<script src="<?php echo base_url();?>/assets/js/neon-demo.js"></script>
 
