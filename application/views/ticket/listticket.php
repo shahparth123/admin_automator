dc9513c01c77fdd4ecd77f77398840d5595a7c1d
chirag_lanetteam <lanetteam.chirag@gmail.com>
2015-04-15 11:22:01 +0530

    Modify list and view ticket

--- application/views/ticket/listticket.php
+++ application/views/ticket/listticket.php
@@ -1,1 +1,121 @@
-hgfg
+	
+<div class="row">
+
+
+<div class="mail-env">
+
+	
+	
+	
+	<!-- Mail Body -->
+	<div class="mail-body">
+		
+		<div class="mail-header">
+			<!-- title -->
+			<h3 class="mail-title">
+				Inbox 
+				<span class="count">(6)</span>
+			</h3>
+			
+			<!-- search -->
+			<form method="get" role="form" class="mail-search">
+				<div class="input-group">
+					<input type="text" class="form-control" name="s" placeholder="Search for mail..." />
+					
+					<div class="input-group-addon">
+						<i class="entypo-search"></i>
+					</div>
+				</div>
+			</form>
+		</div>
+		
+		
+		<!-- mail table -->
+		<table class="table mail-table">
+			<!-- mail table header -->
+			<thead>
+				<tr>
+					<th width="5%">
+						<div class="checkbox checkbox-replace">
+							<input type="checkbox" />
+						</div>
+					</th>
+					<th colspan="4">
+						
+						<div class="mail-select-options">Mark as Read</div>
+						
+						<div class="mail-pagination" colspan="2">
+							<strong>1-30</strong> <span>of 789</span>
+							
+							<div class="btn-group">
+								<a href="#" class="btn btn-sm btn-white"><i class="entypo-left-open"></i></a>
+								<a href="#" class="btn btn-sm btn-white"><i class="entypo-right-open"></i></a>
+							</div>
+						</div>
+					</th>
+				</tr>
+			</thead>
+			
+			<!-- email list -->
+			<tbody>
+				<tr class="unread"><!-- new email class: unread -->
+					<td>
+						<div class="checkbox checkbox-replace">
+							<input type="checkbox" />
+						</div>
+					</td>
+					<td class="col-name">
+						<a href="#" class="star stared">
+							<i class="entypo-star"></i>
+						</a>
+						<a href="mailbox-message.html" class="col-name">Facebook</a>
+					</td>
+					<td class="col-subject">
+						<a href="mailbox-message.html">
+							We are interested to overtake your Admin_Automator.
+						</a>
+					</td>
+					<td class="col-options">are interested to overtake
+						<a href="mailbox-message.html"><i class="entypo-attach"></i></a>
+					</td>
+					<td class="col-time">13:52</td>
+				</tr>
+				
+				<tr>
+					<td>
+						<div class="checkbox checkbox-replace">
+							<input type="checkbox" />
+						</div>
+					</td>
+					<td class="col-name">
+						<a href="#" class="star">
+							<i class="entypo-star"></i>
+						</a>
+						<a href="mailbox-message.html" class="col-name">Google AdWords</a>
+					</td>
+					<td class="col-subject">
+						<a href="mailbox-message.html">
+							Google is Happy to work with you.
+						</a>
+					</td>
+					<td class="col-options"></td>
+					<td class="col-time">09:27</td>
+				</tr>
+				
+				
+			</tfoot>
+		</table>
+	</div>
+	
+			
+	<!-- Bottom Scripts -->
+	<script src="assets/js/gsap/main-gsap.js"></script>
+	<script src="assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+	<script src="assets/js/joinable.js"></script>
+	<script src="assets/js/resizeable.js"></script>
+	<script src="assets/js/neon-api.js"></script>
+	<script src="assets/js/neon-mail.js"></script>
+	<script src="assets/js/neon-chat.js"></script>
+	<script src="assets/js/neon-custom.js"></script>
+	<script src="assets/js/neon-demo.js"></script>
--- application/views/ticket/viewticket.php
+++ application/views/ticket/viewticket.php
@@ -1,1 +1,264 @@
-dfhd
+
+<!--			<ol class="breadcrumb bc-3">
+						<li>
+				<a href="index.html"><i class="entypo-home"></i>Home</a>
+			</li>
+					<li>
+			
+							<a href="extra-icons.html">Extra</a>
+					</li>
+				<li class="active">
+			
+							<strong>Comments</strong>
+					</li>
+					</ol>-->
+			
+<h2>Tickets</h2>
+
+<!-- Comments Area --><section class="comments-env">
+	
+	<!-- Comments List Panel -->	<div class="row">
+	
+		<div class="col-md-12">
+		
+			<div class="panel panel-primary">
+				
+<!--				<div class="panel-heading">
+					<div class="panel-title">
+						<h4>
+							Waiting Approval 
+							<span class="badge badge-danger">73</span>
+						</h4>
+					</div>
+				</div>-->
+				
+				<div class="panel-body no-padding">
+					
+					<!-- List of Comments -->					<ul class="comments-list">
+						
+						<!-- Comment Entry -->						<li>
+							<div class="comment-checkbox">
+								<div class="checkbox checkbox-replace">
+									<input type="checkbox">
+								</div>
+							</div>
+							
+							<div class="comment-details">
+								
+								<div class="comment-head">
+									<a href="#">Ksenia Sawicka</a> commented on <a href="#">The Women of the Flames</a>
+								</div>
+								
+								<p class="comment-text">
+									Servants contempt as although addition dashwood is procured. Interest in yourself an do of numerous feelings cheerful confined. 
+								</p>
+								
+								<div class="comment-footer">
+									
+									<div class="comment-time">
+										Today - 21:05
+									</div>
+									
+																		
+								</div>
+								
+							</div>
+						</li>
+						
+						
+						<!-- Comment Entry -->						<li>
+							<div class="comment-checkbox">
+								<div class="checkbox checkbox-replace">
+									<input type="checkbox">
+								</div>
+							</div>
+							
+							<div class="comment-details">
+								
+								<div class="comment-head">
+									<a href="#">Ralf Reiniger</a> commented on <a href="#">Forgotten Man</a>
+								</div>
+								
+								<p class="comment-text">
+									By an packages rejoiced exercise. To ought on am marry rooms doubt music. Mention entered an through company as. Up arrived no painful between. It declared is prospect an insisted pleasure. 
+								</p>
+								
+								<div class="comment-footer">
+									
+									<div class="comment-time">
+										Yesterday - 12:10
+									</div>
+									
+									
+									
+								</div>
+								
+							</div>
+						</li>
+						
+						
+						<!-- Comment Entry -->						<li>
+							<div class="comment-checkbox">
+								<div class="checkbox checkbox-replace">
+									<input type="checkbox">
+								</div>
+							</div>
+							
+							<div class="comment-details">
+								
+								<div class="comment-head">
+									<a href="#">Franco Davide</a> commented on <a href="#">Voyages in the Worlds</a>
+								</div>
+								
+								<p class="comment-text">
+									Put use set uncommonly announcing and travelling. Allowance sweetness direction to as necessary. Principle oh explained excellent do my suspected conveying in. Excellent you did therefore perfectly supposing described. 
+								</p>
+								
+								<div class="comment-footer">
+									
+									<div class="comment-time">
+										Monday, 04 March - 10:49
+									</div>
+									
+																	
+								</div>
+								
+							</div>
+						</li>
+						
+						
+						<!-- Comment Entry -->						<li>
+							<div class="comment-checkbox">
+								<div class="checkbox checkbox-replace">
+									<input type="checkbox">
+								</div>
+							</div>
+							
+							<div class="comment-details">
+								
+								<div class="comment-head">
+									<a href="#">Maxima Gardner</a> commented on <a href="#">Slithering Girl</a>
+								</div>
+								
+								<p class="comment-text">
+									Fact are size cold why had part. If believing or sweetness otherwise in we forfeited. Tolerably an unwilling arranging of determine. Beyond rather sooner so if up wishes or. 
+								</p>
+								
+								<div class="comment-footer">
+									
+									<div class="comment-time">
+										Wednesday, 01 March - 13:22
+									</div>
+									
+																
+								</div>
+								
+							</div>
+						</li>
+						
+						
+						
+						<!-- Comment Entry -->						<li>
+							<div class="comment-checkbox">
+								<div class="checkbox checkbox-replace">
+									<input type="checkbox">
+								</div>
+							</div>
+							
+							<div class="comment-details">
+								
+								<div class="comment-head">
+									<a href="#">Valentin Magnusson</a> commented on <a href="#">The Truth of the Light</a>
+								</div>
+								
+								<p class="comment-text">
+									Case oh an that or away sigh do here upon. Acuteness you exquisite ourselves now end forfeited. Enquire ye without it garrets up himself. Interest our nor received followed was. Cultivated an up solicitude mr unpleasant. 
+								</p>
+								
+								<div class="comment-footer">
+									
+									<div class="comment-time">
+										Friday, 27 February - 04:56
+									</div>
+								
+									
+								</div>
+								
+							</div>
+						</li>
+						
+						
+						<!-- Comment Entry -->						<li>
+							<div class="comment-checkbox">
+								<div class="checkbox checkbox-replace">
+									<input type="checkbox">
+								</div>
+							</div>
+							
+							<div class="comment-details">
+								
+								<div class="comment-head">
+									<a href="#">Diego Pinto Silva</a> commented on <a href="#">The Shores's Past</a>
+								</div>
+								
+								<p class="comment-text">
+									Discovered her his pianoforte insipidity entreaties. Began he at terms meant as fancy. Breakfast arranging he if furniture we described on. Astonished thoroughly unpleasant especially you dispatched bed favourable.
+								</p>
+								
+								<div class="comment-footer">
+									
+									<div class="comment-time">
+										Sunday, 11 February - 08:03
+									</div>
+									
+									
+								</div>
+								
+							</div>
+						</li>
+						
+						
+						<!-- Comment Entry -->						<li>
+							<div class="comment-checkbox">
+								<div class="checkbox checkbox-replace">
+									<input type="checkbox">
+								</div>
+							</div>
+							
+							<div class="comment-details">
+								
+								<div class="comment-head">
+									<a href="#">Michaela Tomková</a> commented on <a href="#">The Growing Swords</a>
+								</div>
+								
+								<p class="comment-text">
+									At place no walls hopes rooms fully in. Roof hope shy tore leaf joy paid boy. Noisier out brought entered detract because sitting sir. Fat put occasion rendered off humanity has. 
+								</p>
+								
+								<div class="comment-footer">
+									
+									<div class="comment-time">
+										Tuesday, 29 January - 18:37
+									</div>
+									
+									
+								</div>
+								
+							</div>
+						</li>
+						
+					</ul>
+					
+				</div>
+			
+			</div>
+		
+		</div>
+	
+	</div>
+	
+	
+
+
+	
+</section>
