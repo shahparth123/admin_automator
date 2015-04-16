820cb1f747440760a8bcdb19b5f5cec78b53d41b
chirag_lanetteam <lanetteam.chirag@gmail.com>
2015-04-16 12:50:23 +0530

    small change

--- application/controllers/ticket.php
+++ application/controllers/ticket.php
@@ -36,8 +36,7 @@ class Ticket extends CI_Controller {
             $data['permission'] = $role['permission'];
             $data['main_content'] = "ticket/newticket";
             $this->load->view('template/template', $data);
-        }
-        else {
+        } else {
             redirect(base_url() . 'user/login');
         }
     }
@@ -62,45 +61,46 @@ class Ticket extends CI_Controller {
     }
 
     public function viewticket($ticketid = null) {
- if ($this->login_database->is_logged_in()) {
-        $role = $this->session->userdata('logged_in');
-        $userid = $role['id'];
+        if ($this->login_database->is_logged_in()) {
+            $role = $this->session->userdata('logged_in');
+            $userid = $role['id'];
 
-        if ($this->input->post()) {
-            $ticket_id = $this->input->post('ticket_id');
-            $message = $this->input->post('message');
-            $status = $this->input->post('status');
-            //print_r($role);
-            $ticket_id = $this->ticket_model->reply_ticket($userid, $ticket_id, $message, $status);
-            redirect(base_url() . "ticket/viewticket/" . $ticket_id);
-        }
-
-        $data['title'] = "View Ticket";
-        $data['content'] = $this->ticket_model->view_ticket($userid, $ticketid, $role['permission']);
-        $ticket_detail = $this->ticket_model->check_ticket($ticketid, $userid, $role['permission']);
-        $data['isreply'] = 1;
-
-        if (count($data['content']) == 0) {
-            if (count($ticket_detail) == 1) {
-                //print_r($ticket_detail);
-                $content[0]['ticket_id'] = $ticket_detail[0]['id'];
-                $content[0]['subject'] = $ticket_detail[0]['subject'];
-                $content[0]['message'] = $ticket_detail[0]['message'];
-                $content[0]['message'] = $ticket_detail[0]['message'];
-                $content[0]['created_at'] = $ticket_detail[0]['created_at'];
-                $content[0]['is_active'] = $ticket_detail[0]['is_active'];
-                $data['content'] = $content;
-                $data['isreply'] = 0;
-            } else {
-                redirect(base_url());
-                exit;
+            if ($this->input->post()) {
+                $ticket_id = $this->input->post('ticket_id');
+                $message = $this->input->post('message');
+                $status = $this->input->post('status');
+                //print_r($role);
+                $ticket_id = $this->ticket_model->reply_ticket($userid, $ticket_id, $message, $status);
+                redirect(base_url() . "ticket/viewticket/" . $ticket_id);
             }
-        }
-        $data['permission'] = $role['permission'];
-        $data['main_content'] = "ticket/viewticket";
-        $this->load->view('template/template', $data);
-    } else {
+
+            $data['title'] = "View Ticket";
+            $data['content'] = $this->ticket_model->view_ticket($userid, $ticketid, $role['permission']);
+            $ticket_detail = $this->ticket_model->check_ticket($ticketid, $userid, $role['permission']);
+            $data['isreply'] = 1;
+
+            if (count($data['content']) == 0) {
+                if (count($ticket_detail) == 1) {
+                    //print_r($ticket_detail);
+                    $content[0]['ticket_id'] = $ticket_detail[0]['id'];
+                    $content[0]['subject'] = $ticket_detail[0]['subject'];
+                    $content[0]['message'] = $ticket_detail[0]['message'];
+                    $content[0]['message'] = $ticket_detail[0]['message'];
+                    $content[0]['created_at'] = $ticket_detail[0]['created_at'];
+                    $content[0]['is_active'] = $ticket_detail[0]['is_active'];
+                    $data['content'] = $content;
+                    $data['isreply'] = 0;
+                } else {
+                    redirect(base_url());
+                    exit;
+                }
+            }
+            $data['permission'] = $role['permission'];
+            $data['main_content'] = "ticket/viewticket";
+            $this->load->view('template/template', $data);
+        } else {
             redirect(base_url() . 'user/login');
         }
-}
+    }
+
 }
