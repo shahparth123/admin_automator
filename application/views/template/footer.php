ec2f4cefe0c95b9aacebd1f573e83bfee0d56c34
Parth Shah <shahparth123@users.noreply.github.com>
2017-02-16 20:19:37 +0530

    Updated footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -2,6 +2,6 @@
 <footer class="main">
 	
 		
-	&copy; 2014 <strong>LaNet Team</strong> Admin Theme by <a href="http://lanetteam.com" target="_blank">LaNet Team</a>
+	&copy; 2014 <strong>Parth Hosting</strong>
 	
 </footer>
